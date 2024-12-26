cd8083c1dc70efc1611befe5de819a11f0df7136
Michael Bamikunle <mikulobams@gmail.com>
2024-12-26 20:28:57 +0000

    Add NavBar component and restructure layout; remove old page.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "../components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="max-w-3xl mx-auto p-4">
+          <NavBar />
+          {children}
+        </div>
       </body>
     </html>
   );
